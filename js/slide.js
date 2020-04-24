5aef37cc1c35806d3fc5bab2a5b1bc23de46e1e1
Daniel Lima <danieldeveloper1@gmail.com>
2020-04-24 18:49:58 -0300

    mouse events

---
+++ js/slide.js
@@ -0,0 +1,40 @@
+export default class Slide {
+  constructor(slide, wrapper) {
+    this.slide = document.querySelector(slide);
+    this.wrapper = document.querySelector(wrapper);
+  }
+
+  onStart(event) {
+    event.preventDefault();
+    this.wrapper.addEventListener('mousemove', this.onMove);
+  }
+
+  onMove(event) {
+    
+  }
+
+  onEnd(event) {
+    this.wrapper.removeEventListener('mousemove', this.onMove);
+  }
+
+  addSlideEvents() {
+    this.wrapper.addEventListener('mousedown', this.onStart);
+    this.wrapper.addEventListener('mouseup', this.onEnd);
+  }
+
+  /*
+    this referencia ao objeto em si, aqui no caso seria exatamente a div wrapper.
+    o bind faz com que o this seja uma nodelist.
+  */
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+  }
+
+  init() {
+    this.bindEvents();
+    this.addSlideEvents();
+    return this;
+  }
+}
